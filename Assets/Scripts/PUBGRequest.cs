bfce6818c331fe46fb47e63430f139a198bee0d8
Arcant1 <antonioarcuri77@gmail.com>
2020-08-30 20:10:50 -0300

    First Commit

---
+++ Assets/Scripts/PUBGRequest.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityPUBGAPI.Network;
+
+public class PUBGRequest : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject dataTextPrefab;
+    [SerializeField]
+    private GameObject dataContainer;
+    [SerializeField]
+    private GameObject dataViewPort;
+    private readonly string APIKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI3MDQ0NjlhMC1jYTRhLTAxMzgtM2JjOC0yOTY2M2Y3ZDk5YjIiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTk4NTAxOTc1LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImFudG9uaW9hcmN1cmk3In0.W7oU2Xm9yNMX_Aso4YGFOhW3YZGgUleP94SK4silOfQ";
+    private readonly string URL = "https://api.pubg.com/tournaments";
+
+    private void CleanTournamentList()
+    {
+        foreach (Transform item in dataContainer.transform)
+        {
+            Destroy(item.gameObject);
+        }
+    }
+    public void Refresh()
+    {
+        CleanTournamentList();
+        StartCoroutine(GetInfoCorroutine());
+    }
+    private IEnumerator GetInfoCorroutine()
+    {
+        UnityWebRequest webRequest =  UnityWebRequest.Get(URL);
+        webRequest.SetRequestHeader("Authorization", APIKey);
+        webRequest.SetRequestHeader("accept", "application/vnd.api+json");
+        yield return webRequest.SendWebRequest();
+        if (webRequest.isNetworkError || webRequest.isHttpError)
+        {
+            Debug.LogError(webRequest.error);
+        }
+        else
+        {
+            PUBGResponse response = JsonConvert.DeserializeObject<PUBGResponse>(webRequest.downloadHandler.text);
+            foreach (var data in response.Data)
+            {
+                {
+                    var element = Instantiate(dataTextPrefab, dataContainer.transform);
+                    element.transform.localScale = Vector3.one;
+                    element.GetComponent<TextMeshProUGUI>().text = data.Id;
+                }
+                {
+                    var element = Instantiate(dataTextPrefab, dataContainer.transform);
+                    element.transform.localScale = Vector3.one;
+                    element.GetComponent<TextMeshProUGUI>().text = data.Attributes.CreatedAt.ToShortDateString();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PUBGResponse.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public class Attributes
+    {
+        [JsonProperty("createdAt")]
+        public DateTime CreatedAt;
+    }
+
+    public class Datum
+    {
+        [JsonProperty("type")]
+        public string Type;
+
+        [JsonProperty("id")]
+        public string Id;
+
+        [JsonProperty("attributes")]
+        public Attributes Attributes;
+    }
+
+    public class Links
+    {
+        [JsonProperty("self")]
+        public string Self;
+    }
+
+    public class Meta
+    {
+    }
+
+    public class PUBGResponse
+    {
+        [JsonProperty("data")]
+        public List<Datum> Data;
+
+        [JsonProperty("links")]
+        public Links Links;
+
+        [JsonProperty("meta")]
+        public Meta Meta;
+    }
+}
