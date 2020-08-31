efa33cafdf55b93df944b8722671e1de29ef03a8
Arcant1 <antonioarcuri77@gmail.com>
2020-08-31 02:49:28 -0300

    Resolution fixes

--- Assets/Scripts/PUBGRequest.cs
+++ Assets/Scripts/PUBGRequest.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
-using UnityPUBGAPI.Network;
+using static Response;
 
 public class PUBGRequest : MonoBehaviour
 {
@@ -16,6 +16,19 @@ public class PUBGRequest : MonoBehaviour
     private readonly string APIKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI3MDQ0NjlhMC1jYTRhLTAxMzgtM2JjOC0yOTY2M2Y3ZDk5YjIiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTk4NTAxOTc1LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImFudG9uaW9hcmN1cmk3In0.W7oU2Xm9yNMX_Aso4YGFOhW3YZGgUleP94SK4silOfQ";
     private readonly string URL = "https://api.pubg.com/tournaments";
 
+    private void AddData(Datum data)
+    {
+        {
+            var element = Instantiate(dataTextPrefab, dataContainer.transform);
+            element.transform.localScale = Vector3.one;
+            element.GetComponent<TextMeshProUGUI>().text = data.Id;
+        }
+        {
+            var element = Instantiate(dataTextPrefab, dataContainer.transform);
+            element.transform.localScale = Vector3.one;
+            element.GetComponent<TextMeshProUGUI>().text = data.Attributes.CreatedAt.ToShortDateString();
+        }
+    }
     private void CleanTournamentList()
     {
         foreach (Transform item in dataContainer.transform)
@@ -30,7 +43,7 @@ public class PUBGRequest : MonoBehaviour
     }
     private IEnumerator GetInfoCorroutine()
     {
-        UnityWebRequest webRequest =  UnityWebRequest.Get(URL);
+        UnityWebRequest webRequest = UnityWebRequest.Get(URL);
         webRequest.SetRequestHeader("Authorization", APIKey);
         webRequest.SetRequestHeader("accept", "application/vnd.api+json");
         yield return webRequest.SendWebRequest();
@@ -43,17 +56,9 @@ public class PUBGRequest : MonoBehaviour
             PUBGResponse response = JsonConvert.DeserializeObject<PUBGResponse>(webRequest.downloadHandler.text);
             foreach (var data in response.Data)
             {
-                {
-                    var element = Instantiate(dataTextPrefab, dataContainer.transform);
-                    element.transform.localScale = Vector3.one;
-                    element.GetComponent<TextMeshProUGUI>().text = data.Id;
-                }
-                {
-                    var element = Instantiate(dataTextPrefab, dataContainer.transform);
-                    element.transform.localScale = Vector3.one;
-                    element.GetComponent<TextMeshProUGUI>().text = data.Attributes.CreatedAt.ToShortDateString();
-                }
+                AddData(data);
             }
         }
     }
 }
+
